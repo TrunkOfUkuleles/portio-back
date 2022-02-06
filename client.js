ac0ea75f04d1671e51245a636484ce3c45431495
Julien Edwards <60909020+TrunkOfUkuleles@users.noreply.github.com>
2022-02-05 16:53:12 -0800

    basic
    basics

---
+++ client.js
@@ -0,0 +1,28 @@
+'use strict';
+require('dotenv').config();
+
+const io = require('socket.io-client');
+
+// const HOST = process.env.HOST || "http://gifsms-env.eba-pn2gaatk.us-east-2.elasticbeanstalk.com";
+const HOST = process.env.HOSTT || "http://localhost:3004";
+
+const socket = io.connect(HOST + "/gifs");
+
+socket.emit('message', { message: "Test Message", user: "John" });
+
+const user = "Ted";
+const room = "My Private Room";
+
+socket.emit('join', { user, room });
+
+socket.on('user joined', payload => {
+    console.log("User Joined Room: ", payload.user); //Sends notification of user name that join
+})
+
+socket.on('message', payload => {
+    console.log(payload);
+});
+
+socket.on('profile', payload => {
+    console.log("profile: ", payload);
+}
---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3002;
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(PORT, {
+    cors: {
+        origin: "*",
+    }
+});
+
+
+
+
+//Holds participants in each room
+const gifs = io.of('/gifs');
+let allWhoEnter = [];
+
+gifs.on('connection', socket => {
+
+    //Function to have users join rooms
+    socket.on('join', payload => {
+        console.log("Socket:", socket.id);
+
+        //Checks for duplicates in allWhoEnter Array
+        let allWhoEnterDuplicate = allWhoEnter.reduce((acc, userObj) => {
+            if (userObj.socketId === payload.socketId) acc = true;
+            return acc;
+        }, false)
+        //Checks if there are no and pushes user and socket id in array
+        if (!allWhoEnterDuplicate) {
+            allWhoEnter.push({ ...payload.user, socketId: socket.id });
+        }
+        gifs.emit('get all participants', allWhoEnter);
+    });
+
+
+    //listen to new messages from clients
+    socket.on('message', payload => {
+        console.log("HELLO THERE MESSAGE:, ", payload)
+        //push the message to all other clients
+        gifs.emit('message', payload);
+    })
+
+    socket.on('disconnect', payload => {
+        console.log('SocketID: ', socket.id);
+
+        let user = allWhoEnter.filter(userObj => userObj.socketId === socket.id)[0]
+        if (user) {
+            //Removes leaving user from all who enter
+            allWhoEnter = allWhoEnter.filter(user => user.socketId !== socket.id);
+            //Sends all participants to users
+            gifs.emit('get all participants', allWhoEnter);
+            console.log('allWhoEnter', allWhoEnter)
+
+
+            //Emits user left room notification to clients in specific room
+            socket.emit('user disconnected', { user });
+        }
+
+    })
+})
+// otherApp.listen(PORTER, () => console.log(`listening on ${PORTER}`));
+server.listen(3004, () => console.log(`listening on ${3004}`));
